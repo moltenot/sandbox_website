9fd15df38a9b7f45960fc79d8db3711890804229
Theo Molteno <theo@molteno.net>
2023-02-27 22:00:57 +1300

    added vertically centred navbar elements

--- components/navbar.js
+++ components/navbar.js
@@ -1,8 +1,21 @@
-import styles from  "@/styles/Navbar.module.css";
+import styles from "@/styles/Navbar.module.css";
 import Image from "next/image";
-import logo from "../public/logo.png"
+import logo from "../public/black_logo.png";
+import NavLink from "./navlink";
+
 export default function Navbar() {
-  return <div className={`${styles.navbar} full-width`}>
-    <Image src={logo} alt="Sandbox Studios Logo" height={56}/>
-    Hello</div>;
+  const paths = [
+    { text: "TEAM", path: "/team" },
+    { text: "MENU", path: "/menu" },
+    { text: "GEAR", path: "/gear" },
+  ];
+  return (
+    <div className={`${styles.navbar} full-width`}>
+      <div className="max-width-large horizontal-flex">
+        <Image src={logo} alt="Sandbox Studios Logo" height={50} />
+        <div className="flex-pad full-width"/>
+        {paths.map(NavLink)}
+      </div>
+    </div>
+  );
 }
---
+++ components/navlink.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import styles from "@/styles/NavLink.module.css";
+
+export default function NavLink({ text, path }) {
+  return (
+    <>
+      <div className={styles.navLink}>
+        <div className={styles.padder} />
+        <Link href={path}>
+          <div className={styles.textcontainer}>{text}</div>
+        </Link>
+        <div className={styles.padder} />
+      </div>
+    </>
+  );
+}
