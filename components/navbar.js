362f9ba4dffe6cd6082c5dc7217f53aa5fc87fc2
Theo Molteno <theo@molteno.net>
2023-02-27 19:40:02 +1300

    added some image assets

--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,8 @@
 import styles from  "@/styles/Navbar.module.css";
+import Image from "next/image";
+import logo from "../public/logo.png"
 export default function Navbar() {
-  return <div className={`${styles.navbar} full-width`}>Hello</div>;
+  return <div className={`${styles.navbar} full-width`}>
+    <Image src={logo} alt="Sandbox Studios Logo" height={56}/>
+    Hello</div>;
 }
