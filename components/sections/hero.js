0c02cdac8550589baf5243ac9ebc48ec2311d0e6
Theo Molteno <theo@molteno.net>
2023-02-28 22:35:05 +1300

    got a pretty nice hero section going

--- components/sections/hero.js
+++ components/sections/hero.js
@@ -1,17 +1,21 @@
 import heroImage from "@/public/hero.jpg";
 import Image from "next/image";
 import styles from "@/styles/hero.module.css";
+import Link from "next/link";
 export default function Hero() {
   return (
     <div className={styles.root}>
       <div className={styles.container}>
         <div className={`vflex`}>
           <div className="padder" />
-          <div>
+          <div className={styles.textcontainer}>
             <h1>
               <span className={styles.small}>small studio</span> <br />
               MASSIVE SOUND
             </h1>
+            <Link href="tel:0800 788346">
+              <div className={styles.callme}>Call 0800 STUDIO</div>
+            </Link>
           </div>
           <div className="padder" />
         </div>
