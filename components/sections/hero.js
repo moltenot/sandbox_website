a760d99b1b63088bc776ca7173ebbc6f68ba5bcd
Theo Molteno <theo@molteno.net>
2023-02-28 22:15:04 +1300

    got the fonts sorted

--- components/sections/hero.js
+++ components/sections/hero.js
@@ -4,6 +4,18 @@ import styles from "@/styles/hero.module.css";
 export default function Hero() {
   return (
     <div className={styles.root}>
+      <div className={styles.container}>
+        <div className={`vflex`}>
+          <div className="padder" />
+          <div>
+            <h1>
+              <span className={styles.small}>small studio</span> <br />
+              MASSIVE SOUND
+            </h1>
+          </div>
+          <div className="padder" />
+        </div>
+      </div>
       <Image
         src={heroImage}
         className={styles.image}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Krub, Merriweather_Sans } from "next/font/google";
+const krub = Krub({
+  weight: ["200", "300", "400", "500", "600"],
+  style: ["normal"],
+  subsets: ["latin"]
+})
+const merriweather = Merriweather_Sans({
+  weight: ["300", "400", "700", "800"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+      :root {
+          --merriweather-font: ${merriweather.style.fontFamily};
+          --krub-font: ${krub.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
