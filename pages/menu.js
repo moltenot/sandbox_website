42134e412e398c8ebfe4cf056829af1e9adf986b
Theo Molteno <theo@molteno.net>
2023-03-09 02:56:10 +1300

    removed useless padding

--- pages/menu.js
+++ pages/menu.js
@@ -9,12 +9,10 @@ function MenuItem({ name, description, price, tooltip }) {
         <p>{description}</p>
       </div>
       <div className={`vflex ${styles.menuitemright}`}>
-        <div className="flex-pad"/>
+        <div className="flex-pad" />
         <div className={styles.price}>{price}</div>
-        <div className="flex-pad"/>
-        <div className={styles.tooltip}>
-        {tooltip}
-        </div>
+        <div className="flex-pad" />
+        <div className={styles.tooltip}>{tooltip}</div>
       </div>
     </div>
   );
@@ -27,118 +25,129 @@ export default function Menu() {
   return (
     <>
       <Navigation>
-        <h1>Menu</h1>
+        <div className="cont-lg">
+          <h1 className="centred">Menu</h1>
+          <div className="hflex" style={{flexWrap: "wrap", marginLeft:"auto", marginRight:"auto"}}>
+            <div id="food">
+              <h2 className="centred">FOOD</h2>
+              <ol>
+                <li>
+                  <MenuItem
+                    name="SONG - BAND"
+                    description={
+                      "Live recording, editing, mixing and mastering."
+                    }
+                    price={950}
+                  />
+                </li>
+                <li>
+                  <MenuItem
+                    name={"SONG - SOLO"}
+                    description="Live recording, editing, mixing and mastering."
+                    price={800}
+                  />
+                </li>
+                <li>
+                  <MenuItem
+                    name={"VOCAL"}
+                    description="Recording, editing & tuning (per person)."
+                    price={300}
+                  />
+                </li>
+                <li>
+                  <MenuItem
+                    name={"INSTRUMENT"}
+                    description="Recording & editing (per instrument)."
+                    price={200}
+                  />
+                </li>
+              </ol>
 
-        <h2>FOOD</h2>
-        <ol>
-          <li>
-            <MenuItem
-              name="SONG - BAND"
-              description={"Live recording, editing, mixing and mastering."}
-              price={950}
-            />
-          </li>
-          <li>
-            <MenuItem
-              name={"SONG - SOLO"}
-              description="Live recording, editing, mixing and mastering."
-              price={800}
-            />
-          </li>
-          <li>
-            <MenuItem
-              name={"VOCAL"}
-              description="Recording, editing & tuning (per person)."
-              price={300}
-            />
-          </li>
-          <li>
-            <MenuItem
-              name={"INSTRUMENT"}
-              description="Recording & editing (per instrument)."
-              price={200}
-            />
-          </li>
-        </ol>
 
-        <ol>
-          <li>
-            <MenuItem
-              name={"MIX"}
-              description="Master-ready hybrid analog/digital mix.
+              <ol>
+                <li>
+                  <MenuItem
+                    name={"MIX"}
+                    description="Master-ready hybrid analog/digital mix.
 Price per song (<40 stems)."
-              price={550}
-              tooltip={foodToolTip}
-            />
-          </li>
-          <li>
-            <MenuItem
-              name={"MASTER"}
-              description="Preparation of stereo audio file for streaming,
+                    price={550}
+                    tooltip={foodToolTip}
+                  />
+                </li>
+                <li>
+                  <MenuItem
+                    name={"MASTER"}
+                    description="Preparation of stereo audio file for streaming,
 CD, or Vinyl."
-              price={150}
-              tooltip={foodToolTip}
-            />
-          </li>
-        </ol>
+                    price={150}
+                    tooltip={foodToolTip}
+                  />
+                </li>
+              </ol>
+            </div>
 
-        <h2>DRINKS</h2>
-        <ol>
-          <li>
-            <MenuItem
-              name={"COVER ART"}
-              description="Cover art for use on digital platforms."
-              price={350}
-            />
-          </li>
-          <li>
-            <MenuItem
-              name={"PRINT"}
-              description="Artwork formatted for CD or Vinyl, including sleeve."
-              price={350}
-            />
-          </li>
-          <li>
-            <MenuItem
-              name={"BOOKLET"}
-              description="8-page booklet layout for CD innner."
-              price={350}
-            />
-          </li>
-        </ol>
 
-        <ol>
-          <li>
-            <MenuItem
-              name="PHOTOGRAPHY"
-              description={"10 graded hi-res/web-res portrait photos."}
-              price={350}
-            />
-          </li>
-          <li>
-            <MenuItem
-              name="POSTER"
-              description={"Print-ready A3 promotional poster design."}
-              price={150}
-            />
-          </li>
-          <li>
-            <MenuItem
-              name="SOCIALS"
-              description={
-                "Print-ready A3 poster, Social Media post, story & event banner design (Animated +$100)"
-              }
-              price={250}
-            />
-          </li>
-          <li>
-            <MenuItem
-              name="EPK"
-              description={"Layout of Electronic Press Kit document."}
-              price={150}
-            />
-          </li>
-        </ol>
+            <div id="drinks">
+              <h2 className="centred">DRINKS</h2>
+              <ol>
+                <li>
+                  <MenuItem
+                    name={"COVER ART"}
+                    description="Cover art for use on digital platforms."
+                    price={350}
+                  />
+                </li>
+                <li>
+                  <MenuItem
+                    name={"PRINT"}
+                    description="Artwork formatted for CD or Vinyl, including sleeve."
+                    price={350}
+                  />
+                </li>
+                <li>
+                  <MenuItem
+                    name={"BOOKLET"}
+                    description="8-page booklet layout for CD innner."
+                    price={350}
+                  />
+                </li>
+              </ol>
+
+              <ol>
+                <li>
+                  <MenuItem
+                    name="PHOTOGRAPHY"
+                    description={"10 graded hi-res/web-res portrait photos."}
+                    price={350}
+                  />
+                </li>
+                <li>
+                  <MenuItem
+                    name="POSTER"
+                    description={"Print-ready A3 promotional poster design."}
+                    price={150}
+                  />
+                </li>
+                <li>
+                  <MenuItem
+                    name="SOCIALS"
+                    description={
+                      "Print-ready A3 poster, Social Media post, story & event banner design (Animated +$100)"
+                    }
+                    price={250}
+                  />
+                </li>
+                <li>
+                  <MenuItem
+                    name="EPK"
+                    description={"Layout of Electronic Press Kit document."}
+                    price={150}
+                  />
+                </li>
+              </ol>
+            </div>
+          </div>
+        </div>
       </Navigation>
     </>
   );
