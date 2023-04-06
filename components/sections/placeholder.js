c59c502168841d9db87fdfd7a52a46f1aedb4a44
Theo Molteno <theo.molteno@ossability.com>
2023-04-06 12:39:54 +1200

    deleted a bunch of stuff

--- components/sections/placeholder.js
+++
@@ -1,9 +0,0 @@
-
-export default function Placeholder({name}) {
-    return <>
-    <div className="centred" style={{height:"30vw"}}>
-        <h1 >{name}</h1>
-        <p>placeholder section</p>
-    </div>
-    </>
-}
--- pages/contact.js
+++
@@ -1,9 +0,0 @@
-import Navigation from "@/components/navigation";
-
-export default function Contact() {
-    return (
-        <Navigation>
-            <h1>Contact</h1>
-        </Navigation>
-    )
-}
